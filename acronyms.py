793a13ca0d9685a8eeadc738bea58882c542f8b8
Irum Ghafoor <38347944+IrumGhafoor@users.noreply.github.com>
2026-04-28 13:48:40 +0500

    Add files via upload

---
+++ acronyms.py
@@ -0,0 +1,10 @@
+#Python Project 16: Creating Acronyms in Python 
+
+user_input = str(input("Enter a Phrase:"))
+text = user_input.split()
+
+a = " "
+
+for i in text:
+    a = a+str(i[0]).upper()
+print(a)
